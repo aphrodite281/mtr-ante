a1d07367ec76974bf879f6676c60676020e38f6e
aphrodite281 <Aphrodite520@outlook.com>
2025-06-23 14:12:13 +0800

    调整逻辑

--- common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/ScriptHolderBase.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/ScriptHolderBase.java
@@ -219,7 +219,7 @@ public abstract class ScriptHolderBase {
         }
 
         return SCRIPT_THREAD.submit(() -> {
-            long start = System.currentTimeMillis();
+            long start = System.nanoTime();
             failTime = 0;
             try {                
                 Object[] allArgs = new Object[3 + args.length];
@@ -239,7 +239,7 @@ public abstract class ScriptHolderBase {
                 failTime = System.currentTimeMillis();
                 failException = ex;
             }
-            scriptCtx.lastExecuteDuration = System.currentTimeMillis() - start;
+            scriptCtx.lastExecuteDuration = System.nanoTime() - start;
         });
     }
 

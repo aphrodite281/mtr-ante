4aa76f240c0f1311b081d3ae959a4c78cbe58e22
aphrodite281 <Aphrodite520@outlook.com>
2025-06-26 14:41:41 +0800

    try

--- common/src/main/java/cn/zbx1425/mtrsteamloco/mixin/FQHotSpotTruffleRuntimeAccess.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/mixin/FQHotSpotTruffleRuntimeAccess.java
@@ -4,6 +4,7 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,6 +16,23 @@ public abstract class FQHotSpotTruffleRuntimeAccess {
     @Inject(method = "createRuntime", at = @At("HEAD"), cancellable = true, remap = false)
     private static void onCreateRuntime(CallbackInfoReturnable<com.oracle.truffle.api.TruffleRuntime> cir) {
         cir.cancel();
-        cir.setReturnValue(new com.oracle.truffle.api.impl.DefaultTruffleRuntime("ANTE Shield"));
+        try {
+            // HotSpotThreadLocalHandshake.initializePendingOffset();
+            // HotSpotFastThreadLocal.ensureLoaded();
+
+            Class<?> clazz = Class.forName("jdk.graal.compiler.truffle.hotspot.HotSpotTruffleCompilationSupport");
+            com.oracle.truffle.compiler.TruffleCompilationSupport compilationSupport = (com.oracle.truffle.compiler.TruffleCompilationSupport) (Object) clazz.getConstructor().newInstance();
+            com.oracle.truffle.runtime.hotspot.HotSpotTruffleRuntime rt = new com.oracle.truffle.runtime.hotspot.HotSpotTruffleRuntime(compilationSupport);
+            // registerVirtualThreadMountHooks();
+            compilationSupport.registerRuntime(rt);
+            cir.setReturnValue(rt);
+        } catch (Exception e) {
+            e.printStackTrace();
+            cir.setReturnValue(new com.oracle.truffle.api.impl.DefaultTruffleRuntime("ANTE Shield"));
+        }
     }
+
+    // @Shadow(remap = false)
+    // private static void registerVirtualThreadMountHooks() {
+    // }
 }
